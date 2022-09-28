37c6afffb237c365f4b4e68af963f5f3b1e898c1
castro william <williamcastrov@gmail.com>
2022-09-28 13:51:30 -0500

    actualiza

--- src/components/sales/SaveInvoiceSiigo.js
+++ src/components/sales/SaveInvoiceSiigo.js
@@ -156,111 +156,118 @@ function SaveInvoiceSiigo(props) {
             let control = 0;
             if (facturas.id_fact == numeroPedido) {
 
-                //var fecha = new Date();
-                let date = new Date(facturas.fechafactura);
-                //let fecha = String(date.getFullYear() + '-' + String(date.getMonth() + 1).padStart(2, '0') + '-' + date.getDate()).padStart(2, '0');
-                //contador = contador + 1;
+                if (facturas.idcliente != 0) {
 
-                let valor = 0;
-                let valorpayment = 0;
-                let valoritem = 0;
-                let descuento = 0;
-                let idiva = 0;
-                let valoriva = 0;
+                    //var fecha = new Date();
+                    let date = new Date(facturas.fechafactura);
+                    //let fecha = String(date.getFullYear() + '-' + String(date.getMonth() + 1).padStart(2, '0') + '-' + date.getDate()).padStart(2, '0');
+                    //contador = contador + 1;
 
-                listDetalleFacturas && listDetalleFacturas.map((items, row) => {
-                    control = control + 1;
+                    let valor = 0;
+                    let valorpayment = 0;
+                    let valoritem = 0;
+                    let descuento = 0;
+                    let idiva = 0;
+                    let valoriva = 0;
 
-                    if (items.pedido == facturas.id_fact) {
-                        console.log("ITEMS PEDIDOS : ", items)
+                    listDetalleFacturas && listDetalleFacturas.map((items, row) => {
+                        control = control + 1;
 
-                        lisProductosSiigo && lisProductosSiigo.map((productos, row) => {
-                            if (productos.sku == items.variant_sku) {
-                                idiva = productos.idiva;
-                                valoriva = 1 + (productos.porcetajeiva / 100);
-                            }
-                        })
+                        if (items.pedido == facturas.id_fact) {
+                            console.log("ITEMS PEDIDOS : ", items)
 
-                        //if (items.tipoimpuesto == 745) {
+                            lisProductosSiigo && lisProductosSiigo.map((productos, row) => {
+                                if (productos.sku == items.variant_sku) {
+                                    idiva = productos.idiva;
+                                    valoriva = 1 + (productos.porcetajeiva / 100);
+                                }
+                            })
 
-                        //valoritem = (valoritem + (valor - (facturas.descuento * -1))).toFixed(4);
-                        valoritem = valoritem + (items.price / 1.19)
-                        descuento = 0;
-                        /*} else {
-                            valoritem = items.price;
-                            valoritem = (items.price - (facturas.descuento * -1)).toFixed(0);
-                            valorpayment = valoritem;
+                            //if (items.tipoimpuesto == 745) {
+
+                            //valoritem = (valoritem + (valor - (facturas.descuento * -1))).toFixed(4);
+                            valoritem = valoritem + (items.price / 1.19)
                             descuento = 0;
-                        }*/
+                            /*} else {
+                                valoritem = items.price;
+                                valoritem = (items.price - (facturas.descuento * -1)).toFixed(0);
+                                valorpayment = valoritem;
+                                descuento = 0;
+                            }*/
 
-                    }
-                })
-                valor = (valoritem * 1.19).toFixed(0);
-                valorpayment = valorpayment + facturas.valorfactura;
-                valoritem = (valoritem).toFixed(4);
-
-                // descuento = (((items.price-(items.price / 1.19))/items.price)*100).toFixed(4);
-                let comentario = "API - " + facturas.id_fact
-                const leer = async () => {
-                    let params = {
-                        id: 1397,
-                        date: fechaactual,
-                        identification: facturas.idcliente,
-                        cost_center: facturas.cost_center,
-                        seller: facturas.seller,
-                        //code: items.codigoproductosiigo,
-                        //description: items.advert_name,
-                        //quantity: items.quantity,
-                        discount: descuento,
-                        price: valoritem,
-                        value: valorpayment,
-                        idpayments: 7500,
-                        due_date: fechaactual,
-                        idtaxes: idiva,
-                        observations: comentario,
-                        pedido: facturas.id_fact,
-                        iva: valoriva
-                    };
-                    console.log("DATOS A FACTURAR : ", params)
-
-                    await axios({
-                        method: 'post',
-                        url: 'https://sitbusiness.co/cyclewear/api/731', params
-                    }).then(res => {
-                        let numfact = 0;
-
-                        console.log("DATOS RESPONSE : ", res)
-                        console.log("NUMERO FACTURA : ", res.data.id)
-                        if (res.data.status == 200) {
-                            setNumeroFactura(res.data.id);
-                            let mensaje = "Factura de venta N. : " + res.data.id;
-                            setTituloMensajes(mensaje)
-                            abrirCerrarModal(true);
-                        } else {
-                            let cadena = res.data.Error;
-                            let posicion = cadena.search(':');
-                            let valor = cadena.slice(posicion + 1);
-                            let inicial = cadena.slice(0, posicion);
-                            
-                            let mensaje;
-                            if (inicial == "The code doesn't exist")
-                                mensaje = "Codigo de producto no existe : " + valor;
-                            else
-                                if (inicial == "The customer doesn't exist")
-                                    mensaje = "Tercero no existe : " + valor;
-                                else
-                                    mensaje = "Error creando factura "
-
-                            setTituloMensajes(mensaje)
-                            abrirCerrarModal(true);
                         }
-                        setLoading(false);
-                    }
-                    ).catch(function (error) {
-                        console.log("ERROR LEYENDO FACTURAS");
                     })
+                    valor = (valoritem * 1.19).toFixed(0);
+                    valorpayment = valorpayment + facturas.valorfactura;
+                    valoritem = (valoritem).toFixed(4);
+
+                    // descuento = (((items.price-(items.price / 1.19))/items.price)*100).toFixed(4);
+                    let comentario = "API - " + facturas.id_fact
+                    const leer = async () => {
+                        let params = {
+                            id: 1397,
+                            date: fechaactual,
+                            identification: facturas.idcliente,
+                            cost_center: facturas.cost_center,
+                            seller: facturas.seller,
+                            //code: items.codigoproductosiigo,
+                            //description: items.advert_name,
+                            //quantity: items.quantity,
+                            discount: descuento,
+                            price: valoritem,
+                            value: valorpayment,
+                            idpayments: 7500,
+                            due_date: fechaactual,
+                            idtaxes: idiva,
+                            observations: comentario,
+                            pedido: facturas.id_fact,
+                            iva: valoriva
+                        };
+                        console.log("DATOS A FACTURAR : ", params)
+
+                        await axios({
+                            method: 'post',
+                            url: 'https://sitbusiness.co/cyclewear/api/731', params
+                        }).then(res => {
+                            let numfact = 0;
+
+                            console.log("DATOS RESPONSE : ", res)
+                            console.log("NUMERO FACTURA : ", res.data.id)
+                            if (res.data.status == 200) {
+                                setNumeroFactura(res.data.id);
+                                let mensaje = "Factura de venta N. : " + res.data.id;
+                                setTituloMensajes(mensaje)
+                                abrirCerrarModal(true);
+                            } else {
+                                let cadena = res.data.Error;
+                                let posicion = cadena.search(':');
+                                let valor = cadena.slice(posicion + 1);
+                                let inicial = cadena.slice(0, posicion);
+
+                                let mensaje;
+                                if (inicial == "The code doesn't exist")
+                                    mensaje = "Codigo de producto no existe : " + valor;
+                                else
+                                    if (inicial == "The customer doesn't exist")
+                                        mensaje = "Tercero no existe : " + valor;
+                                    else
+                                        mensaje = "Error creando factura "
+
+                                setTituloMensajes(mensaje)
+                                abrirCerrarModal(true);
+                            }
+                            setLoading(false);
+                        }
+                        ).catch(function (error) {
+                            console.log("ERROR LEYENDO FACTURAS");
+                        })
+                    }
+                    leer();
+                }else{
+                    let mensaje = "Error, Cedula esta en CERO ";
+                    setTituloMensajes(mensaje)
+                    abrirCerrarModal(true);
                 }
-                leer();
             }
         })
     }
